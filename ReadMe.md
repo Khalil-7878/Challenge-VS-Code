f0a9f231b9ea17b64c8d8d6f947e8475c1a09893
khalil abo-hashesh <khalilaliabwhsesh12@gmail.com>
2026-09-02 14:18:27 +0300

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Challenge VS Code
+
+this is a represnt a challenge in programming advice
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Student: Unknown" << endl;
+    cout << "Course: Git and GitHub" << endl;
+    
+    return 0;
+}
